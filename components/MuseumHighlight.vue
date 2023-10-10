13f9fa68f7d633f5297ec3293d6e340989fbfd5b
Ryan <klhong124@gmail.com>
2023-10-10 16:11:11 +0100

    project init

---
+++ components/MuseumHighlight.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="museum-highlight">
+        testing
+        <!-- Display the available information for the highlight -->
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'MuseumHighlight',
+    components: {
+
+    },
+    mixins: [
+    ],
+    props: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    computed: {
+        newsDate() {
+            // Highlight's news item date
+        },
+    },
+    methods: {
+
+    },
+    created() {
+
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.museum-highlight {
+    @apply text-red-500;
+
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app-layout">
+      <main>
+        <nav class="nav">
+            <Logo class="nav__logo"/>
+            <div class="nav__title">National History Museum</div>
+            <button class="nav__button">
+                Menu
+            </button>
+        </nav>
+       <slot />
+      </main>
+    </div>
+  </template>
+
+  <style lang="scss" scoped>
+
+    .nav {
+        display: flex;
+        align-items: center;
+        padding: 2px 8%;
+        background: grey;
+
+        &__logo{
+            margin: 0 12px;
+        }
+
+        &__button{
+            margin-left: auto;
+        }
+    }
+  </style>
